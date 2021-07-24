08fb5fd406ae0a3096e1326c3294c8b6c894dfee
Kxrie <jaydenzhang5139@icloud.com>
2021-07-24 21:27:16 +1000

    Class for generating basic GraphQL queries

---
+++ src/main/java/GraphQLQuery.java
@@ -0,0 +1,56 @@
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+public class GraphQLQuery {
+    /**
+     * Returns a JsonObject of the GraphQL Query to obtain the API call rate limits.
+     * @return Returns a JsonObject of the GraphQL Query to obtain the API call rate limits.
+     */
+    static JsonObject rateLimitQuery(){
+        String query = """
+        {
+          rateLimitData {
+            limitPerHour
+            pointsResetIn
+            pointsSpentThisHour
+          }
+        }
+        """;
+        return toJson(query);
+    }
+
+    static JsonObject reportInfoQuery(String code) {
+        String query = String.format("""
+        {
+          reportData{
+            report(code: "%s"){
+              startTime
+              fights{
+                id
+                bossPercentage
+                fightPercentage
+                encounterID
+                lastPhase
+                lastPhaseIsIntermission
+                startTime
+                endTime
+              }
+            }
+          }
+        }
+        """, code);
+        return toJson(query);
+
+    }
+
+    /**
+     * Converts a string to json.
+     * @param query The query in string format.
+     * @return JsonObject form of the query.
+     */
+    static JsonObject toJson(String query) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("query", query);
+        return obj;
+    }
+}
