fb38dc7329c74fed99de1f6ef6e0786dec701fc1
Kxrie <jaydenzhang5139@icloud.com>
2021-07-24 21:26:17 +1000

    Basic custom console outputs

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,4 +1,8 @@
 public class Console {
+    /**
+     * Prints a message on the console which contains a linebreak sequence.
+     * @param msg The message to print to the console.
+     */
     public static void print(String... msg) {
         for (String message : msg) {
             System.out.println(message);
@@ -6,6 +10,11 @@ public class Console {
         System.out.println("-----");
     }
 
+    /**
+     * Prints a message on the console which can contain a linebreak sequence.
+     * @param linebreak Boolean value on whether a linebreak should be printed.
+     * @param msg The message to print to the console.
+     */
     public static void print(Boolean linebreak, String... msg) {
         for (String message : msg) {
             System.out.println(message);
